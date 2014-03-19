12126948c31d9733fe8c31382fbdef3cd328b1bc
Josh Starcher <josh.starcher@gmail.com>
2014-03-19 11:21:54 -0400

    username => relay_username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |user_name, password|
       begin
         @linker = IdentityLinker::Linker.new(user_name, password)
-        @linker.find_linked_identity('username','foo@example.com','ssoguid')
+        @linker.find_linked_identity('relay_username','foo@example.com','ssoguid')
         true
       rescue IdentityLinker::Error => e
         false if e.to_s.strip == 'bad username'
